dd4880d75a0fb06f4e51000fa3d01175777c4a3f
shy-dell <76672306+shy-dell@users.noreply.github.com>
2025-04-04 12:53:35 -0400

    "Update div loop to create rows then children"

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,10 +1,16 @@
 // create divs
 const container = document.querySelector('#container');
 document.addEventListener('DOMContentLoaded', () => {
-    for (let i=0; i < 256;i++) {
+    for (let i=1; i <= 16;i++) {
         const newDiv = document.createElement('div');
+        newDiv.classList.add(`row${i}`)
         newDiv.style.border = '1px solid black';
         container.appendChild(newDiv);
-        console.log('created a new div')
+        for (let n = 1; n <=16; n++) {
+            const childContainer = document.querySelector(`.row${i}`);
+            const childDiv = document.createElement('div');
+            childDiv.style.border = '1px solid black';
+            childContainer.appendChild(childDiv);
+        }
     }
 });
