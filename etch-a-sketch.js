f405a8d0aea839d1e865bcdf9da964b3007778af
shy-dell <76672306+shy-dell@users.noreply.github.com>
2025-04-05 17:34:46 -0400

    remove duplicate borders on rows

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -11,7 +11,6 @@ document.addEventListener('DOMContentLoaded', () => {
     for (let i=1; i <= 16;i++) {
         const newDiv = document.createElement('div');
         newDiv.classList.add(`row${i}`)
-        newDiv.style.border = '1px solid black';
         container.appendChild(newDiv);
         for (let n = 1; n <=16; n++) {
             const childContainer = document.querySelector(`.row${i}`);
