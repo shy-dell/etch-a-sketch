90586d394839c2fe7acf97fe271c0a6c6fde32e2
shy-dell <76672306+shy-dell@users.noreply.github.com>
2025-04-05 21:17:57 -0400

    Remove DOMLoadContent event listener
    The DOMLoadContent event listener was causing any code outside of the function to be run prior to building the table, therefore could not call any mouse event listeners

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,22 +1,22 @@
 // create divs
 const container = document.querySelector('#container');
-document.addEventListener('DOMContentLoaded', () => {
-    // create header
-    const bodyElement = document.body
-    const header = document.createElement('h1');
-    header.classList.add('titleHeader');
-    header.textContent = "Etch-a-Sketch";
-    bodyElement.insertBefore(header,container);
 
-    for (let i=1; i <= 16;i++) {
-        const newDiv = document.createElement('div');
-        newDiv.classList.add(`row${i}`)
-        container.appendChild(newDiv);
-        for (let n = 1; n <=16; n++) {
-            const childContainer = document.querySelector(`.row${i}`);
-            const childDiv = document.createElement('div');
-            childDiv.style.border = '1px solid black';
-            childContainer.appendChild(childDiv);
-        }
-    }
-});
+// create header
+const bodyElement = document.body
+const header = document.createElement('h1');
+header.classList.add('titleHeader');
+header.textContent = "Etch-a-Sketch";
+bodyElement.insertBefore(header,container);
+
+for (let i=1; i <= 16;i++) {
+    const newDiv = document.createElement('div');
+    newDiv.classList.add(`row${i}`)
+    container.appendChild(newDiv);
+    for (let n = 1; n <=16; n++) {
+        const childContainer = document.querySelector(`.row${i}`);
+        const childDiv = document.createElement('div');
+        childDiv.classList.add('square');
+        childDiv.style.border = '1px solid black';
+        childContainer.appendChild(childDiv);
+    };
+};
